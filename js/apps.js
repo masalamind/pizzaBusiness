5470b1c0bdaad44b5b2ce1aec980e0d4d9522d98
masalamind <themasalacollective@gmail.com>
2022-03-18 15:08:31 +0300

    Finished basic business logic for pizza object

--- js/apps.js
+++ js/apps.js
@@ -1,65 +1,64 @@
-var saucyToppings = ["BBQSauce","MexicanSauce","Sweet&SourSauce","SweetChilliSauce","TikkaSauce","FriedUpSauce",];
-var greenyToppings = ["GreenChilli","Olives","Garlic","Tomato","Pineapple","Mushroom","GreenPepper","Onion","SweetBellPepper","FreshAvocado","Jalapeno"];
-var meatyToppings = ["Macon", "Chicken","Ground-beef","Salami","Ham","SlicedCheese"];
-
-
 
 var prices = {
 
-    type:{
-       
-        "VeggiePizza": {"large": 950, "medium": 750, "small": 550}, 
-        "PepperoniPizza": {"large": 950, "medium": 750, "small": 550}, 
-        "PizzaCapricciosa": {"large": 950, "medium": 750, "small": 550}, 
-        "MargheritaPizza": {"large": 850, "medium": 650, "small": 400}, 
-        "BBQChickenPizza": {"large": 950, "medium": 750, "small": 550}, 
-        "HawaiianPizza": {"large": 1050, "medium": 800, "small": 550}, 
-        "BuffaloPizza": {"large": 1200, "medium": 850, "small": 650}, 
-        "SicilianPizza": {"large": 1050, "medium": 800, "small": 600}, 
-        "CapresePizza": {"large": 1000, "medium": 850, "small": 550}, 
-        "NewYorkPizza": {"large": 950, "medium": 750, "small": 550},
-        "PizzaMarinara": {"large": 1000, "medium": 750, "small": 550}, 
-        "PizzaNapoloetana": {"large": 950, "medium": 750, "small": 550} 
+     type:{
+
+        "VeggiePizza": { "large": 950, "medium": 750, "small": 550 }, 
+        "PepperoniPizza": { "large": 950, "medium": 750, "small": 550 }, 
+        "PizzaCapricciosa": { "large": 950, "medium": 750, "small": 550 }, 
+        "MargheritaPizza": { "large": 850, "medium": 650, "small": 400 }, 
+        "BBQChickenPizza": { "large": 950, "medium": 750, "small": 550 }, 
+        "HawaiianPizza": { "large": 1050, "medium": 800, "small": 550 }, 
+        "BuffaloPizza": { "large": 1200, "medium": 850, "small": 650 }, 
+        "SicilianPizza": { "large": 1050, "medium": 800, "small": 600 }, 
+        "CapresePizza": { "large": 1000, "medium": 850, "small": 550 }, 
+        "NewYorkPizza": { "large": 950, "medium": 750, "small": 550 },
+        "PizzaMarinara": { "large": 1000, "medium": 750, "small": 550 }, 
+        "PizzaNapoloetana": { "large": 950, "medium": 750, "small": 550 } 
     },
 
     
     crust:{
+
         "thinNCrispy": 150, 
         "thick": 150, 
         "doubleStack": 350,
-        "doubleStackSmall": 200,
-    
-    },
+        "doubleStackSmall": 200    
+    }
 
-    
-        // categorize the toppings first then 
-    
 
 };
 
 
 
-class Pizza{
+var saucyToppings = [ "BBQSauce","MexicanSauce","Sweet&SourSauce","SweetChilliSauce","TikkaSauce","FriedUpSauce" ];
+var greenyToppings = [ "GreenChilli","Olives","Garlic","Tomato","Pineapple","Mushroom","GreenPepper","Onion","SweetBellPepper","FreshAvocado","Jalapeno" ];
+var meatyToppings = [ "Macon", "Chicken","Ground-beef","Salami","Ham","SlicedCheese" ];
 
-    constructor(type,size,crust,toppings){
+
+class Pizza{    
+
+    constructor(type,size,crust,toppings,quantity=1){
+
         this.type = type;              // initial value is empty: filled when a user chooses
         this.size = size;               //[smallPizza,mediumPizza,largePizza],
         this.crust = crust;   // default crust is thin and crispy   
         this.toppings = toppings;
+        this.quantity = quantity;
 
-        }  
+    }  
        
     
-    fetchToppingsPrice(){
+    fetchToppingsPrice(){        
 
         var toppingsPrice = {
-            "saucy":{ "large":100, "medium": 100 , "small":50}, 
-            "greeny":{ "large": 150, "medium": 100, "small": 50},
-            "meaty":{ "large": 150, "medium": 150 , "small": 100}
-    
+
+            "saucy":{ "large":100, "medium": 100 , "small":50 }, 
+            "greeny":{ "large": 150, "medium": 100, "small": 50 },
+            "meaty":{ "large": 150, "medium": 150 , "small": 100 }    
         } 
 
-        var currentToppingsPrice;
+        let currentToppingsPrice;
         
         if(saucyToppings.includes(this.toppings)){
           
@@ -84,15 +83,28 @@ class Pizza{
         
         let pizzaPrice = prices.type[this.type][this.size];
         let crustPrice = prices.crust[this.crust];
-
+        let toppingsPrice = this.fetchToppingsPrice();       
         
-       
-        console.log(pizzaPrice);
-        console.log(crustPrice); 
-        console.log(this.fetchToppingsPrice());
         
+        
+        // careful that toppingsPrice does not clash with the other variable 
+        console.log(this.quantity)
+        return (pizzaPrice + crustPrice + toppingsPrice) * this.quantity;
         
     }
+
+    increasePizzasQty(){
+
+        this.quantity += 1;
+    }
+
+    decreasePizzasQty(){
+
+        this.quantity -= 1;
+
+    }
+
+    
 };
 
 
